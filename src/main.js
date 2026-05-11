8d70c305a01ab9f225288d9a7f15d428c45a8699
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-05-11 09:37:40 +0200

    refactor: split style.css into exercises, flashcards, settings, and notes partials

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import './styles/exercises.css'
+import './styles/flashcards.css'
+import './styles/settings.css'
+import './styles/notes.css'
 import App from './App.vue'
 import router from './router/index.js'
 
